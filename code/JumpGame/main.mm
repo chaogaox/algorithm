8fad5c71972759ea2ae82fd35c984705521b9f92
chaogaox <296260642@qq.com>
2020-09-15 14:20:27 +0800

    修改代码

--- code/JumpGame/main.mm
+++ code/JumpGame/main.mm
@@ -14,10 +14,9 @@
 using namespace std;
 
 
-int minSteps1()
+int minSteps1(int *nums,int size)
 {
-    int nums[] = {2,3,1,2,4,2,1};
-    int position = sizeof(nums)/sizeof(nums[0]) - 1;
+    int position = size - 1;
     int steps = 0;
     while (position > 0) {
         for (int i = 0; i < position; i++) {
@@ -32,13 +31,12 @@ int minSteps1()
 }
 
 
-int minSteps2()
+int minSteps2(int *nums,int size)
 {
-    int nums[] = {2,3,1,2,4,2,1};
     int currMaxIdx = 0;   // 当前跳跃范围最远坐标
     int nextMaxIdx = 0;   // 下一个跳跃范围最远坐标
     int steps = 0;        // 步数
-    int target = sizeof(nums)/sizeof(nums[0]) - 1;
+    int target = size - 1;
     for (int i = 0; i <= currMaxIdx; ++i) {   // 在当前跳跃范围内遍历，获得下次跳跃能到达的最远距离
         nextMaxIdx = max(nextMaxIdx, i + nums[i]);
         if (nextMaxIdx >= target){
@@ -53,10 +51,12 @@ int minSteps2()
 }
 
 int main(int argc, char * argv[]) {
-    int step = minSteps1();
+    int nums[] = {2,3,1,1,4,2,1};
+    int size = sizeof(nums)/sizeof(nums[0]);
+    int step = minSteps1(nums,size);
     NSLog(@"最少步数：%d",step);
-    
-    int step2 = minSteps2();
+
+    int step2 = minSteps2(nums,size);
     NSLog(@"最少步数：%d",step2);
 
 }
